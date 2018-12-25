1cc6ccfe2ea49945ad9f22c7a5e2cb8ea7a3cd7b
dezhizhang <1541609448@qq.com>
2018-12-25 10:04:44 +0800

    添加路由

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-
 class HomeController extends Controller {
   async index() {
-    this.ctx.body = 'hi, egg';
+     this.ctx.body = 'hello egg.js';
+
   }
 }
 
 module.exports = HomeController;
+
